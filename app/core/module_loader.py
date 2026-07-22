1eefa2c5c7ba629fa9f9b0bb1cf512f7e2eedb6f
Fabio <arte3d429@gmail.com>
2026-07-22 20:54:21 -0300

    PF-0008 Module Loader Core

---
+++ app/core/module_loader.py
@@ -0,0 +1,29 @@
+from app.modules.documents.module import DocumentsModule
+
+
+class ModuleLoader:
+
+
+    def __init__(self, registry):
+
+        self.registry = registry
+
+
+
+    def load_modules(self):
+
+        modules = [
+
+            DocumentsModule()
+
+        ]
+
+
+        for module in modules:
+
+            self.registry.register(
+                module.get_info()
+            )
+
+
+        return modules
